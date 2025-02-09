6c7ec36cd92b2ca053313df3425caf82a5a89d28
NasirSultan <rainasirsultan123@gmail.com>
2025-02-09 15:33:15 +0500

    f

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
 
 const App = () => {
-  const handleRegister = (token) => {
-    console.log("Registered with token:", token);
-    // Handle token or user state as needed
-  };
-
-  const handleLogin = (token) => {
-    console.log("Logged in with token:", token);
-    // Handle token or user state as needed
-  };
-
   return (
-    <Router>
-      <div className="p-4">
-        
-        <Routes>
-          <Route path="/" element={<Register onRegister={handleRegister} />} />
-          <Route path="/login" element={<Login onLogin={handleLogin} />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="p-4">
+      <h1>Welcome to My App</h1>
+      <p>This is a simple default setup.</p>
+    </div>
   );
 };
 
